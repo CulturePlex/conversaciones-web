823c6d274f5676d5b9a5504cf8e6d41d3f527918
Luis <luis@cplexstudio1.arts.uwo.ca>
2025-03-31 11:33:25 -0400

    Curso 3

---
+++ wp-content/plugins/learnpress-co-instructor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c5feb6e0fd20bf81ff928a3569dcaad02a21f9e5',
+        'name' => 'learnpress-co-instructor/learnpress-co-instructor',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'learnpress-co-instructor/learnpress-co-instructor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c5feb6e0fd20bf81ff928a3569dcaad02a21f9e5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
